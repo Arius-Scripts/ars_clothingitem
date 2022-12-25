890c96c69ddd93ef552e91c2f2cc0eda8a6f6bb1
Arius <70983185+Arius-Development@users.noreply.github.com>
2022-12-25 20:05:10 +0100

    Update fxmanifest.lua

--- ars_clothingitem/fxmanifest.lua
+++ ars_clothingitem/fxmanifest.lua
@@ -2,6 +2,9 @@ fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
+author "Arius Development"
+description "Item based clothing"
+version "1.0.0"
 
 client_scripts {"client/*.lua", "shop/client/*.lua"}
 server_scripts {'@oxmysql/lib/MySQL.lua', "shop/server/*.lua"}
